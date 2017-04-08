90108455e4ca3a10dcb8c2c155caa969cc51ae60
Mandalapu <mandalap@usc.edu>
2017-04-09 00:54:51 +0100

    Implemented Basic Slot Filling Algorithm

---
+++ src/ConstructQuery.java
@@ -0,0 +1,187 @@
+import java.util.*;
+
+/**
+ * This class is responsible to build the query using the tag-parsing algorithm. It first constructs the partial parts
+ * and then combines them to form the whole query.
+ * @author Manu
+ *
+ */
+public class ConstructQuery {
+
+	//This lists are used through out the class, for now use the single copy to construct the query.
+	//All the functions uses this lists for constructing.
+	private List<String> words = new ArrayList<>();
+	private List<String> tags = new ArrayList<>();
+	private Map<String, Node> nodes = new HashMap<>();
+	//This set contains all the attribute names and relational entities that are found in the sentence.
+	//This words can be used to include all the table names in the FromClause.
+	private Set<String> fromClauseNodes = new HashSet<>();
+	
+	
+	public ConstructQuery(List<String> words, List<String> tags, Map<String, Node> resultNodes)
+	{
+		this.words = words;
+		this.tags = tags;
+		this.nodes = resultNodes;
+	}
+	/**
+	 * @return the query which is constructed.
+	 */
+	public String getQueryFromTags()
+	{
+		/**
+		 * Algorithm: get the index of the complex condition, and then using the LN tags construct the blocks 
+		 * of the complex condition.
+		 */
+		int length = tags.size();
+		int conditionClauseStartIndex = length, index = 0;
+		//TODO Need to find a better solution for finding the start of complex condition.
+		for( index = length - 1; index >= 0; index-- )
+		{
+			//The first RN from last in the tag list is used as a start point for complex condition.
+			if( tags.get(index).equals("RN") )
+			{
+				conditionClauseStartIndex = index + 1;
+				break;
+			}
+		}
+		String conditionClause = getComplexCondition(conditionClauseStartIndex, length - 1);
+		String selectClause = getSelectClause(0, index);
+		StringBuilder query = new StringBuilder();
+		query.append(selectClause).append(conditionClause);
+		return query.toString();
+	}
+	
+	/**
+	 * This function is responsible to construct the complex condition, it may be combination of many combinations.
+	 * @param startIndex, index from which the WHERE clause starts from the given German sentence.
+	 * @param endIndex
+	 * @return the complex condition constructed, empty when there are no valid combinations.
+	 */
+	private String getComplexCondition(int startIndex, int endIndex)
+	{
+		String condition = new String();
+		StringBuilder complexCondition = new StringBuilder();
+		for( int index = endIndex; index >= startIndex; index-- )
+		{
+			//Need to see how the list can be seperated if there is no LN itsel
+			if( tags.get(index).equals("LN") )
+			{
+				condition = getCondition( index + 1, endIndex);
+				//TODO need to be careful with the spaces before and after the logical operator.
+				complexCondition.append(words.get(index)).append(condition).append(" ");
+				endIndex = index - 1;
+				break;
+			}
+		}
+		condition = getCondition(startIndex, endIndex);
+		complexCondition.insert(0, condition).insert(0, "WHERE ");
+		return complexCondition.toString();
+	}
+	
+	
+	/**
+	 * 
+	 * @param startIndex
+	 * @param endIndex
+	 * @return
+	 */
+	private String getCondition(int startIndex, int endIndex)
+	{
+		StringBuilder condition = new StringBuilder();
+		//TODO need to include all valid conditions.
+		if( (endIndex - startIndex) < 2 )
+			return condition.toString();
+		String leftTreeComp = "", rightTreeComp = "", comparator = "";
+		int numberOfAttributeNodes = 0;
+
+		for( int index = endIndex; index >= startIndex; index-- )
+		{
+			//In future, you need to get the value of the node this ON is mapped to.
+			if( tags.get(index).equals("ON") )
+			{
+				//This should be modified according the node information of the ON.
+				comparator = " = ";
+			}
+			else if( tags.get(index).equals("VN") || tags.get(index).equals("NTW"))
+			{
+				//Need to consider the case of integers in the sentence. The other information of the VN should also
+				//considered  while integrating them in the condition.
+				rightTreeComp = words.get(index);
+			}
+			else if( tags.get(index).equals("RN") )
+			{
+				//You need to get the default attribute key of this node which is returned from the word net.
+				//TODO assign the default attribute key.
+				Node curNode = nodes.get(words.get(index));
+				fromClauseNodes.add(curNode.value);
+				leftTreeComp = curNode.defaultAttribbuteKey;
+			}
+			else if( tags.get(index).equals("AN") )
+			{
+				numberOfAttributeNodes++;
+				//All the AN's and RN's should have a reserved node containing all the information.
+				Node curNode = nodes.get(words.get(index));
+				fromClauseNodes.add(curNode.parentName);
+				if( numberOfAttributeNodes == 1 )
+				{
+					leftTreeComp = curNode.value;
+				}
+				else if( numberOfAttributeNodes == 2 )
+				{
+					rightTreeComp = leftTreeComp;
+					leftTreeComp = curNode.value;
+				}
+			}
+		}
+		condition.append(leftTreeComp).append(comparator).append(rightTreeComp);
+		return condition.toString();
+	}
+	
+	/**
+	 * This function is responsible for constructing the Sclause in the SQL query. It needs to consider all the
+	 * default attribute nodes and their parents while constructing the clause.
+	 * @param startIndex
+	 * @param endIndex
+	 * @return
+	 */
+	private String getSelectClause(int startIndex, int endIndex)
+	{
+		//You need to parse the list of nodes
+		System.out.println("The start and end Indexes that"
+				+ " for the select clause function are:" +startIndex + " "+endIndex);
+		StringBuilder sClause = new StringBuilder();
+		StringBuilder retrieve = new StringBuilder();
+		sClause.append("SELECT ");
+		//if it is an AN, you can insert its parentName.attribute name in the select portion
+		//if it is an RN, it's default attribute key should be inserted in this portion.
+		for( int index = 0; index <= endIndex; index++ )
+		{
+			//get the required information required to construct the query.
+			Node curNode = nodes.get(words.get(index));
+			if( tags.get(index).equals("RN") )
+			{
+				fromClauseNodes.add(curNode.value);
+				retrieve.append(curNode.value + "." + curNode.defaultAttribbuteKey);
+			}
+			else if( tags.get(index).equals("AN") )
+			{
+				fromClauseNodes.add(curNode.parentName);
+				retrieve.append(curNode.parentName + "." + curNode.value);
+			}
+			if( index != endIndex )
+				retrieve.append(", ");
+		}
+		sClause.append(retrieve).append( " FROM ");
+		//Now we need to insert all the parentNodes, whose attributes are encountered in the condtion or
+		//any where in the sentence.
+		Iterator<String> setIterator = fromClauseNodes.iterator();
+		StringBuilder fromClause = new StringBuilder();
+		while( setIterator.hasNext() )
+		{
+			fromClause.append(setIterator.next().toString()).append(" , ");
+		}
+		sClause.append(fromClause);
+		return sClause.toString();
+	}
+}
--- src/Solution.java
+++ src/Solution.java
@@ -3,78 +3,108 @@ import edu.stanford.nlp.pipeline.*;
 import edu.stanford.nlp.trees.*;
 import edu.stanford.nlp.util.CoreMap;
 import edu.stanford.nlp.util.StringUtils;
-
 import java.util.*;
+import java.util.Map.Entry;
 
 public class Solution {
 
     public static void main(String[] args) {
-        SQLTranslator sqlTranslator = new SQLTranslator("Liste Freunde, die als Informatik-Ingenieur von beruf, auf");
-        sqlTranslator.parse();
+        //SQLTranslator sqlTranslator = new SQLTranslator("Liste Freunde, die als Informatik-Ingenieur von beruf, auf");
+    	SQLTranslator sqlTranslator = new SQLTranslator("Liste Freunde, die an USC studiert haben und jetzt bei Expedia "
+    			+ "arbeiten, auf");
+    	sqlTranslator.parse();
     }
 }
 
 class SQLTranslator {
 
     private String text;
-    private ArrayList<String> stopWords = new ArrayList( Arrays.asList( new String[]{"die", "auf","der","das","sich","für", "mit", "welche", "den", "aus", "jetzt", "bei", "von" } ) );
+    //We use the list of stop words which do not have any information for constructing the SQL query.  
+    private ArrayList<String> stopWords = new ArrayList(Arrays.asList( new String[]{"haben", "die", "auf","der","das","sich","für", "mit", "welche", "den", "aus", "jetzt", "von" } ) );
     private String punctutations = ".,:;?";
+    //TODO need to add more logical operators and words that closely mean like logical operator.
     private HashMap<String, String> logicalOperator = new HashMap<>();
     private HashMap<String, ArrayList<String >> relationalOperator = new HashMap<>();
+    //The three data structures below holds the tags and Node information for each non-stop word in the
+    //given Query sentence in NL.
+    private Map<String, Node> resultNodes = new HashMap<>();
     private ArrayList<String> wordList = new ArrayList<>();
     private ArrayList<String> tagList = new ArrayList<>();
+    //Local word net that is built for serving the purpose of actual wordnet.
     private WordNet wordNet = new WordNet();
     private boolean isRelational;
 
-    SQLTranslator(String text) {
+    //Need to add more sentences in future into "text"
+    public SQLTranslator(String text) {
+    	//text contains the sentence that needs to be converted as SQL query.
         this.text = text;
+        this.logicalOperator.put("und","AND");
+        this.logicalOperator.put("oder","OR");
+        //List of relational operator and their possible word list in german language.
+        this.relationalOperator.put(">",new ArrayList( Arrays.asList( new String[]{"älter", "über"} )));
+        this.relationalOperator.put("=",new ArrayList( Arrays.asList( new String[]{"bei", "in", "am","an","als"} )));
+        this.relationalOperator.put("<",new ArrayList( Arrays.asList( new String[]{"jünger", "unter"} )));
     }
 
     public void parse() {
-        logicalOperator.put("und","AND");
-        logicalOperator.put("oder","OR");
-        relationalOperator.put(">",new ArrayList( Arrays.asList( new String[]{"älter", "über"} )));
-        relationalOperator.put("=",new ArrayList( Arrays.asList( new String[]{"in", "am","an","als"} )));
-        relationalOperator.put("<",new ArrayList( Arrays.asList( new String[]{"jünger", "unter"} )));
+    	
         Annotation germanAnnotation = new Annotation(text);
         Properties germanProperties = StringUtils.argsToProperties(
                 new String[]{"-props", "StanfordCoreNLP-german.properties"});
         StanfordCoreNLP pipeline = new StanfordCoreNLP(germanProperties);
         pipeline.annotate(germanAnnotation);
-        for (CoreMap sentence : germanAnnotation.get(CoreAnnotations.SentencesAnnotation.class)) {
+        for (CoreMap sentence : germanAnnotation.get(CoreAnnotations.SentencesAnnotation.class))
+        {
             Tree sentenceTree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
             //GrammaticalStructureFactory gsf = new UniversalEnglishGrammaticalStructureFactory();
             //SemanticGraph dependencyGraph = SemanticGraphFactory.generateUncollapsedDependencies(sentenceTree);
             //.generateCollapsedDependencies( gsf.newGrammaticalStructure(sentenceTree), GrammaticalStructure.Extras.NONE );
             System.out.println(sentenceTree.labeledYield());
-            for(int i=0;i<sentenceTree.labeledYield().size();i++) {
+            for(int i=0;i<sentenceTree.labeledYield().size();i++)
+            {
                 String[] words = sentenceTree.labeledYield().get(i).toString().split("/");
-                if(!stopWords.contains(words[0]) && !punctutations.contains(words[0])) {
-                    if (words[1].equals("KON")) {
+                if(!stopWords.contains(words[0]) && !punctutations.contains(words[0]))
+                {
+                    if (words[1].equals("KON")) 
+                    {
+                    	//This is where we need to form a Node in the future.
                         wordList.add(logicalOperator.get(words[0]));
                         tagList.add("LN");
-                    } else if (words[1].equals("NE") || words[1].equals("CARD")) {
+                    }
+                    else if (words[1].equals("NE") || words[1].equals("CARD")) 
+                    {
                         wordList.add(words[0]);
                         tagList.add("VN");
-                    } else {
+                    }
+                    else
+                    {
                         isRelational = false;
                         Iterator it = relationalOperator.entrySet().iterator();
-                        while (it.hasNext()) {
-                            Map.Entry pair = (Map.Entry) it.next();
+                        while (it.hasNext())
+                        {
+                            Map.Entry pair = (Entry) it.next();
                             ArrayList<String> array = (ArrayList<String>) pair.getValue();
-                            if(array.contains(words[0])) {
+                            if(array.contains(words[0]))
+                            {
                                 wordList.add((String) pair.getKey());
                                 tagList.add("ON");
                                 isRelational = true;
                                 break;
                             }
                         }
-                        if(!isRelational) {
+                        if(!isRelational)
+                        {
                             Node node = wordNet.synSet(words[0],words[1]);
-                            if(node!=null) {
+                            if(node!=null)
+                            {
                                 wordList.add(words[0]);
                                 tagList.add(node.nodeType);
-                            } else {
+                                //This is where you need to add the node into the hashmap, use this information
+                                //for building the query.
+                                resultNodes.put(words[0], node);
+                            }
+                            else
+                            {
                                 wordList.add(words[0]);
                                 tagList.add("NTW");
                             }
@@ -84,6 +114,21 @@ class SQLTranslator {
             }
             System.out.println(wordList.toString());
             System.out.println(tagList.toString());
+            String finalQuery = constructQuery();
+            System.out.println("The final resultant Query constructed is:" +finalQuery);
         }
     }
+    
+    /**
+     * Creates a ConstructQuery class instance and  then uses it to build the query.
+     * @return
+     */
+    private String constructQuery()
+    {
+    	ConstructQuery queryConstructor = new ConstructQuery(wordList, tagList, resultNodes);
+    	//Return the query constructed by the queryConstructor instance.
+    	return queryConstructor.getQueryFromTags();
+    	//return "";
+    }
+    
 }
